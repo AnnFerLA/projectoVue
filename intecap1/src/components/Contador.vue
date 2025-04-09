ec6f38a6c80d381ebc4eecca83f5fbb5395b33ac
AnnFerLA <fernandolarrazabalalvarado@gmail.com>
2025-04-08 20:48:50 -0600

    projecto modificado para eliminar archivos generados automaticamente por vue, y carga del nuevo archivo Contador.vue para nueva interfaz

---
+++ intecap1/src/components/Contador.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Hola mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+ </template>
+
+<script setup>
+import { ref } from 'vue';
+const contador = ref(1);
+const incrementar = () => {
+  contador.value++;
+  if (contador.value === 10) {
+    contador.value = 0;
+}
+}
+
+const decrementar = () => {
+  contador.value--;
+}
+
+</script>
+
+<style scoped>
+p{
+  color: blueviolet;
+  text-align: center;
+}
+h1{
+  color: aquamarine;
+  text-align: center;
+}
+button{
+  color: black;
+  text-align: center;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+</style>
